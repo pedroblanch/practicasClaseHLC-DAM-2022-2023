841e343f6c71252db93ef4c5c1d194835249a7db
pedroblanch <pedroblanch@iesjulioverne.es>
2022-10-17 08:37:21 +0200

    Lunes 17 de octubre

--- practica8-ion-select/src/app/home/home.page.ts
+++ practica8-ion-select/src/app/home/home.page.ts
@@ -10,13 +10,23 @@ export class HomePage {
 
   private asignaturas:string[]=['HLC','MÓVILES','ACCESO A DATOS'];
   private alumnos:string[]=['Perez López, Marta', 'García Aranda, José', 'Nieto Blanco, Ana'];
-  private alumnosAsignaturas: AlumnoAsignatura[]=[];
+  private alumnosSeleccionados:string[];
+  private alumnosAsignaturas: AlumnoAsignatura[];
   private asignaturaSeleccionada:string;
 
   constructor() {}
 
   cambiaAsignatura(event){
     console.log(event.target.value);
+    console.log(this.asignaturaSeleccionada);
   }
 
-}
+  seleccionadosAlumnos(event){
+    console.log(this.alumnosSeleccionados);
+    this.alumnosAsignaturas=[];
+    this.alumnosSeleccionados.forEach((alumno:string)=>{
+      this.alumnosAsignaturas.push(new AlumnoAsignatura(alumno,this.asignaturaSeleccionada));
+    })
+  }
+
+}//end_class
