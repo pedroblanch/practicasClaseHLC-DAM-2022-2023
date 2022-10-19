46dd43c219914779a515f85c4f4a85276fb23912
ppblanchleiva <ppblanchleiva@gmail.com>
2022-10-19 21:31:57 +0200

    Miércoles 19 de octubre - 2

--- practica9Unidad2/src/app/home/home.page.ts
+++ practica9Unidad2/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { LineaDetalle } from '../modelo/LineaDetalle';
 import { Producto } from '../modelo/Producto';
 
 @Component({
@@ -9,41 +10,32 @@ import { Producto } from '../modelo/Producto';
 })
 export class HomePage {
 
-  private productoSeleccionado:Producto;
-  private productos:Producto[]=[];
+  private productoSeleccionado: Producto;
+  private codigoSeleccionado: number;
+  private productos: Producto[] = [];
+  private lineasDetalle: LineaDetalle[] = [];
 
-  constructor(public alertController:AlertController) {
-    this.productos.push(new Producto(1,"Producto 1",10.3));
-    this.productos.push(new Producto(2,"Producto 2",25.3));
-    this.productos.push(new Producto(3,"Producto 3",65));
-    this.productos.push(new Producto(4,"Producto 4",109.98));
-    this.productos.push(new Producto(5,"Producto 5",45.7));
+  constructor(public alertController: AlertController) {
+    this.productos.push(new Producto(1, "Producto 1", 10.3));
+    this.productos.push(new Producto(2, "Producto 2", 25.3));
   }
 
-  seleccionadoProducto(event){
-    console.log(this.productoSeleccionado);
+  seleccionadoProducto(event) {
+    //console.log(event.target.value);
+    if (!this.codigoSeleccionado)
+      return;
+    this.productos.forEach(producto => {
+      if (producto.codigo == this.codigoSeleccionado) {
+        this.productoSeleccionado = producto;
+      }
+    });
     this.alertUnidadesProducto();
   }
 
   async alertUnidadesProducto() {
     const alert = await this.alertController.create({
-      header: this.productoSeleccionado.codigo + " - " + this.productoSeleccionado.descripcion,
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log("Cancelado");
-          },
-        },
-        {
-          text: 'OK',
-          role: 'confirm',
-          handler: (data) => {
-            console.log(this.productoSeleccionado.descripcion + "-" +  data['unidades']);
-          },
-        },
-      ],      inputs: [
+      header: this.productoSeleccionado.codigo + ' - ' + this.productoSeleccionado.descripcion,
+      inputs: [
         {
           type: 'number',
           name: 'unidades',
@@ -51,6 +43,46 @@ export class HomePage {
           min: 1
         }
       ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            //borro la selección del ion-select
+            this.codigoSeleccionado = null;
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: (data) => {
+            let encontrado = false;
+            let unidades = Number(data['unidades']);
+            this.lineasDetalle.forEach((linea) => {
+              if (linea.codigo == this.productoSeleccionado.codigo) {
+                //ya está añadido el producto
+                encontrado = true;
+                //actualizo las unidades y el total
+                linea.unidades += unidades;
+                linea.total = linea.pvp * linea.unidades;
+              }
+            });
+            if (!encontrado) {
+              //el producto no ha sido aún añadido
+              let lineaDetalle = new LineaDetalle(
+                this.productoSeleccionado.codigo,
+                this.productoSeleccionado.descripcion,
+                this.productoSeleccionado.pvp,
+                unidades,
+                this.productoSeleccionado.pvp * unidades
+              );
+              this.lineasDetalle.push(lineaDetalle);
+            }
+            //borro la selección del ion-select
+            this.codigoSeleccionado = null;
+          },
+        }
+      ]
     });
 
     await alert.present();
