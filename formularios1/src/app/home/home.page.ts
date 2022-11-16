d0e3442609fc257ce1191902aa7e37af2ef1fd76
pedroblanch <pedroblanch@iesjulioverne.es>
2022-11-16 13:24:12 +0100

    Miércoles 16 de noviembre

--- formularios1/src/app/home/home.page.ts
+++ formularios1/src/app/home/home.page.ts
@@ -13,6 +13,7 @@ export class HomePage implements OnInit {
 
   validations_form: FormGroup;
   genders: Array<string>;
+  matching_passwords_group: FormGroup;
 
   constructor(public formBuilder: FormBuilder,
     private navCtrl: NavController) {
@@ -27,8 +28,21 @@ export class HomePage implements OnInit {
       "Female"
     ];
 
+    this.matching_passwords_group = new FormGroup({
+      password: new FormControl('', Validators.compose([
+        Validators.maxLength(15),
+        Validators.minLength(8),
+        Validators.pattern('^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.,-]).*$'),
+        Validators.required
+      ])),
+      confirmPassword: new FormControl('', Validators.required)
+    }, (formGroup: FormGroup) => {
+      return this.passwordRepetida(formGroup);
+    });
+
     this.validations_form = this.formBuilder.group({
       username: new FormControl('', Validators.compose([
+        this.userNameRepetido,
         Validators.maxLength(25),
         Validators.minLength(5),
         Validators.pattern('^[a-zA-Z]{1}[a-zA-Z0-9]+$'),
@@ -36,18 +50,35 @@ export class HomePage implements OnInit {
       ])),
       name: new FormControl('', Validators.required),
       lastname: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.compose([
+      email: new FormControl('pedroblanch@gmail.com', Validators.compose([
         Validators.required,
         Validators.pattern('^[a-zA-Z0-9_.+-]+[@]{1}[a-zA-Z0-9-]+[.]{1}[a-zA-Z]+$')
       ])),
       gender: new FormControl(this.genders[0], Validators.required),
-      terms: new FormControl(false, Validators.pattern('true'))
+      terms: new FormControl(false, Validators.pattern('true')),
+      matching_passwords: this.matching_passwords_group
     });
   }//end_ngOnInit
 
+  passwordRepetida(fg: FormGroup) {
+    if (fg.controls['password'].value != fg.controls['confirmPassword'].value)
+      return ({ passwordRepetida: true });  //no se valida
+    else
+      return (null);  //si se valida
+  }
+
+  userNameRepetido(fc: FormControl) {
+    if (fc.value.toLowerCase() === "abc123" || fc.value.toLowerCase() === "cba321") {
+      return ({ userNameRepetido: true });  //no se valida
+    } else {
+      return (null);  //se valida
+    }
+  }
+
+
   onSubmit(values) {
-    let user:User=new User(values.username,
-      values.name,values.lastname,values.email,values.gender,values.terms);
+    let user: User = new User(values.username,
+      values.name, values.lastname, values.email, values.gender, values.terms);
     let navigationExtras: NavigationExtras = {
       queryParams: {
         user: JSON.stringify(user),
