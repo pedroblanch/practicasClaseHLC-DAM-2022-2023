2a5ba008f027ab922ad450c6a54676116e293d27
pedroblanch <pedroblanch@iesjulioverne.es>
2022-11-23 13:27:22 +0100

    Miércoles 23 de noviembre

--- formularios1/src/app/home/home.page.ts
+++ formularios1/src/app/home/home.page.ts
@@ -15,6 +15,41 @@ export class HomePage implements OnInit {
   genders: Array<string>;
   matching_passwords_group: FormGroup;
 
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'Username is required.' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters, and must begin with a letter.' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
+    ],
+    'name': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'Last name is required.' }
+    ],
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Please enter a valid email.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 8 characters long.' },
+      { type: 'maxlength', message: 'Password must be less than or equal to 15 characters.' },
+      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, one number and one of this characters: . - ;' }
+    ],
+    'confirmPassword': [
+      { type: 'required', message: 'Confirm password is required.' }
+    ],
+    'matching_passwords': [
+      { type: 'passwordRepetida', message: 'Password mismatch.' }
+    ],
+    'terms': [
+      { type: 'pattern', message: 'You must accept terms and conditions.' }
+    ],
+  };
+
   constructor(public formBuilder: FormBuilder,
     private navCtrl: NavController) {
 
